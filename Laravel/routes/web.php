c1832c846485495108ac3864f1eb54a387843031
tata <taynassilva3456@gmail.com>
2025-04-22 16:58:29 -0300

    Update

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pagina1');
 });
 
 Route::get('/teste', [TestController::class, 'teste'])->name('teste');
+
+Route::post('/pagina2', function () {
+    return view('pagina2');
+});
